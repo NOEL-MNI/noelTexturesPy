e96b26f7470b92466c74929dbac411518c309885
Ravnoor Gill <ravnoor@gmail.com>
2019-05-14 18:16:01 -0400

    n4 before registration

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM neuressence/pynoel-gui-fast:dash
+MAINTAINER ravnoorgill <ravnoor@gmail.com>
+
+WORKDIR /noelpy
+
+COPY Procfile /noelpy
+COPY templates/* /noelpy/templates/
+COPY utils.py /noelpy
+COPY image_processing_utils.py /noelpy
+COPY dash_flaskapp.py /noelpy
+
+EXPOSE 9999
+
+# ENTRYPOINT [ "gunicorn", "--access-logfile", "-", "--log-file", "./logs.log", "dash_flaskapp:server", "-b", ":9999" ]
+ENTRYPOINT [ "python3", "dash_flaskapp.py" ]
---
+++ utils.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import ants
+from collections import Counter
+
+def write_nifti(input, id, output_dir, type):
+    output_fname = os.path.join(output_dir, id + '_' + type + '.nii.gz')
+    ants.image_write( input, output_fname)
+
+def compute_RI(image, bg, mask):
+    ri = np.zeros_like(image)
+    bgm = np.stack(np.where(np.logical_and(image < bg, mask == 1)), axis=1)
+    bgm_ind = bgm[:,0], bgm[:,1], bgm[:,2]
+    bgp = np.stack(np.where(np.logical_and(image > bg, mask == 1)), axis=1)
+    bgp_ind = bgp[:,0], bgp[:,1], bgp[:,2]
+
+    ri[bgm_ind] = 100 * (1 - (bg - image[bgm_ind]) / bg )
+    ri[bgp_ind] = 100 * (1 + (bg - image[bgp_ind]) / bg )
+    return ri
+
+def peakfinder(gm, wm, lower_q, upper_q):
+    gm_peak = Counter(threshold_percentile(gm, lower_q, upper_q)).most_common(1)[0][0]
+    wm_peak = Counter(threshold_percentile(wm, lower_q, upper_q)).most_common(1)[0][0]
+    bg = 0.5 * (gm_peak + wm_peak)
+    return bg
+
+def threshold_percentile(x, lower_q, upper_q):
+    x = x.numpy()
+    lq = np.percentile(x, lower_q)
+    uq = np.percentile(x, upper_q)
+    x = x[np.logical_and(x>lq, x<=uq)]
+    return x.flatten()
