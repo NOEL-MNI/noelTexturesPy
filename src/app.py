25e7e5f4de8bae0d287949052b0658317f6e99ad
Ravnoor Gill <ravnoor.gill@mcgill.ca>
2024-12-26 10:16:32 -0500

    refactor(ruff): linting, formatting, organize imports - modularize logging

--- src/app.py
+++ src/app.py
@@ -1,97 +1,110 @@
-import base64, glob, logging, os, shutil, sys
+#!/usr/bin/env python
+
+import base64
+import glob
+import logging
+import os
+import sys
 from urllib.parse import quote as urlquote
 
-from flask import Flask, send_from_directory
 import dash_bootstrap_components as dbc
-from dash import Dash, callback, html, dcc, Input, Output, State, MATCH, ALL
+from dash import Dash
+from dash import Input
+from dash import Output
+from dash import State
+from dash import callback
+from dash import html
+from flask import Flask
+from flask import send_from_directory
+from noelTexturesPy.image_processing import case_id as random_case_id
+from noelTexturesPy.image_processing import log_filename
+from noelTexturesPy.image_processing import logger
+from noelTexturesPy.image_processing import noelTexturesPy
+from noelTexturesPy.layout import body
+from noelTexturesPy.layout import jumbotron
 
-from image_processing import noelTexturesPy, logger
-from utils import find_logger_basefilename, random_case_id
+template = os.path.join('./templates', 'mni_icbm152_t1_tal_nlin_sym_09a.nii.gz')
 
-log_filename = find_logger_basefilename(logger)
-logger.info("log filename: {}".format(str(log_filename)))
+TEMPDIR = os.environ.get('TEMPDIR')
+output_dir = os.path.join(TEMPDIR, 'outputs')
+upload_directory = os.path.join(TEMPDIR, 'uploads')
 
-template = os.path.join("./templates", "mni_icbm152_t1_tal_nlin_sym_09a.nii.gz")
-output_dir = os.path.join("./outputs")
-
-UPLOAD_DIRECTORY = os.path.join("./uploads")
-
-try:
-    shutil.rmtree(UPLOAD_DIRECTORY)
-    shutil.rmtree(output_dir)
-except OSError as e:
-    print("Warning: %s - %s." % (e.filename, e.strerror))
-
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
+if not os.path.exists(upload_directory):
+    os.makedirs(upload_directory)
     os.makedirs(output_dir)
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# Normally, Dash creates its own Flask server internally. By creating our own,
+
+# normally, Dash creates its own Flask server internally - by creating our own,
 # we can create a route for downloading files directly:
 server = Flask(__name__)
-# app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 app = Dash(server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = "noelTexturesPy"
+app.title = 'noelTexturesPy'
 
 # initialize a logger to prevent flask from logging to console
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 app.layout = html.Div([jumbotron, body])
-app.config["suppress_callback_exceptions"] = True
+app.config['suppress_callback_exceptions'] = True
+app.logger.disabled = True
 app.css.append_css(
-    {"external_url": "https://use.fontawesome.com/releases/v5.8.2/css/all.css"}
+    {'external_url': 'https://use.fontawesome.com/releases/v5.8.2/css/all.css'}
 )
 
 
 def save_file(name, content):
     """Decode and store a file uploaded with Plotly Dash."""
-    data = content.encode("utf8").split(b";base64,")[1]
-    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
+    data = content.encode('utf8').split(b';base64,')[1]
+    with open(os.path.join(upload_directory, name), 'wb') as fp:
         fp.write(base64.decodebytes(data))
 
 
 def uploaded_files():
     """List the files in the upload directory."""
     files = []
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        path = os.path.join(UPLOAD_DIRECTORY, filename)
-        if os.path.isfile(path) and path.endswith(("nii.gz", "nii")):
+    for filename in os.listdir(upload_directory):
+        path = os.path.join(upload_directory, filename)
+        if os.path.isfile(path) and path.endswith(('nii.gz', 'nii')):
             files.append(filename)
     return files
 
 
+@server.route('/download/<path:path>')
+def download(path):
+    """Serve a file from the upload directory."""
+    return send_from_directory(output_dir, path, as_attachment=True)
+
+
 def file_download_link(filename):
     """Create a Plotly Dash 'A' element that downloads a file from the app."""
-    location = "/download/{}".format(urlquote(filename))
+    location = '/download/{}'.format(urlquote(filename))
     return html.A(filename, href=location)
 
 
 def list_files(output_dir=output_dir):
     f = []
-    for dirpath, dirnames, filenames in os.walk(output_dir):
+    for _, _, filenames in os.walk(output_dir):
         for name in filenames:
-            if name.endswith(("nii", "pdf")):
+            if name.endswith(('nii', 'pdf')):
                 f.extend(filenames)
     return sorted(set(f))
 
 
 @callback(
-    Output("case-id-output", "children"),
-    [Input("case-id-input", "n_blur")],
-    [State("case-id-input", "value")],
+    Output('case-id-output', 'children'),
+    [Input('case-id-input', 'n_blur')],
+    [State('case-id-input', 'value')],
 )
 def output_text(nb, value):
     return value
 
 
 @callback(
-    Output("file-list", "children"),
+    Output('file-list', 'children'),
     [
-        Input("upload-data", "filename"),
-        Input("upload-data", "contents"),
-        Input("case-id-output", "children"),
+        Input('upload-data', 'filename'),
+        Input('upload-data', 'contents'),
+        Input('case-id-output', 'children'),
     ],
 )
 def update_output(
@@ -100,7 +113,7 @@ def update_output(
     case_id,
     output_dir=output_dir,
     template=template,
-    uploads_dir=UPLOAD_DIRECTORY,
+    uploads_dir=upload_directory,
 ):
     """Save uploaded files and regenerate the file list."""
 
@@ -114,48 +127,48 @@ def update_output(
         # return [html.Li("No files generated yet!")]
         return [
             dbc.Button(
-                [dbc.Spinner(size="sm", type="grow"), " No files generated yet! "],
-                color="success",
+                [dbc.Spinner(size='sm', type='grow'), ' No files generated yet! '],
+                color='success',
                 disabled=True,
             )
         ]
     elif len(files) == 1 or len(files) == 2:
         t1, t2 = None, None
         for file in files:
-            if file.lower().find("t1") != -1:
+            if file.lower().find('t1') != -1:
                 t1 = os.path.join(uploads_dir, file)
-                logger.info("T1-weighted image detected: {}".format(file))
-                print("T1-weighted image detected: {}".format(file))
-            if file.lower().find("t2") != -1 or file.lower().find("flair") != -1:
+                logger.info('T1-weighted image detected: {}'.format(file))
+                print('T1-weighted image detected: {}'.format(file))
+            if file.lower().find('t2') != -1 or file.lower().find('flair') != -1:
                 t2 = os.path.join(uploads_dir, file)
-                logger.info("T2-weighted image detected: {}".format(file))
-                print("T2-weighted image detected: {}".format(file))
+                logger.info('T2-weighted image detected: {}'.format(file))
+                print('T2-weighted image detected: {}'.format(file))
     else:
-        print("more than 2 modalities are not supported at this time")
-        logger.warn("more than 2 modalities are not supported at this time")
+        print('more than 2 modalities are not supported at this time')
+        logger.warning('more than 2 modalities are not supported at this time')
 
     # id = output_text()
     if case_id is not None:
-        logger.info("Case ID: {}".format(str(case_id)))
+        logger.info('Case ID: {}'.format(str(case_id)))
     else:
-        case_id = random_case_id()
-        logger.info("assigning randomly generated case ID")
-        logger.info("case ID: {}".format(case_id))
+        case_id = random_case_id
+        logger.info('assigning randomly generated case ID')
+        logger.info('case ID: {}'.format(case_id))
     noelTexturesPy(
         id=str(case_id),
         t1=t1,
         t2=t2,
         output_dir=output_dir,
+        temp_dir=TEMPDIR,
         template=template,
         usen3=False,
     ).file_processor()
 
     try:
-        for f in glob.glob(os.path.join(uploads_dir, "*")):
+        for f in glob.glob(os.path.join(uploads_dir, '*')):
             os.remove(f)
-        # os.remove('./logs.log')
     except OSError as e:
-        print("Warning: %s - %s." % (e.filename, e.strerror))
+        print('Warning: %s - %s.' % (e.filename, e.strerror))
 
     outputs = list_files(output_dir=output_dir)
 
@@ -163,42 +176,41 @@ def update_output(
         html.Ul(
             html.I(
                 file_download_link(filename),
-                className="fas fa-arrow-alt-circle-down fa-sm fa-fw",
+                className='fas fa-arrow-alt-circle-down fa-sm fa-fw',
             ),
-            className="fa-ul",
+            className='fa-ul',
             style={
-                "margin": "-20px",
-                "textAlign": "left",
-                "padding-left": "10px",
-                "padding-right": "10px",
+                'margin': '-20px',
+                'textAlign': 'left',
+                'padding-left': '10px',
+                'padding-right': '10px',
             },
         )
         for filename in outputs
     ]
 
 
-@callback(Output("div-out", "children"), [Input("interval1", "n_intervals")])
+@callback(Output('div-out', 'children'), [Input('interval1', 'n_intervals')])
 def update_interval(n):
     orig_stdout = sys.stdout
-    f = open(log_filename, "a")
+    f = open(log_filename, 'a')
     sys.stdout = f
-    # print('Intervals Passed: {}'.format(str(n)))
     sys.stdout = orig_stdout
     f.close()
-    return "Logs"
+    return 'Logs'
 
 
-@callback(Output("console-out", "srcDoc"), [Input("interval2", "n_intervals")])
-def update_output(n):
-    file = open(log_filename, "r")
-    data = ""
+@callback(Output('console-out', 'srcDoc'), [Input('interval2', 'n_intervals')])
+def update_console(n):
+    file = open(log_filename, 'r')
+    data = ''
     lines = file.readlines()
     if lines.__len__() <= 14:
         last_lines = lines
     else:
         last_lines = lines[-14:]
     for line in last_lines:
-        data = data + line + "<BR>"
+        data = data + line + '<BR>'
     file.close()
     return data
 
